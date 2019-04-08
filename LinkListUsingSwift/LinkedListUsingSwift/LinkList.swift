d5ce4716983c9858991163fe783fb132f4522b2b
Varun Kumar <varun.kumar@dineout.co.in>
2019-04-08 13:26:00 +0530

    func append, insert, removefirst, remove last implemented. WIP test

--- LinkListUsingSwift/LinkedListUsingSwift/LinkList.swift
+++ LinkListUsingSwift/LinkedListUsingSwift/LinkList.swift
@@ -13,9 +13,8 @@ class LinkedList<T>: CustomStringConvertible where T:Comparable, T:CustomStringC
    // Operations in LinkedList
     // 1: insert 2: insertatposition 3: append 4: removefirst 5 removelast 6: removefrom position 7: search
     // Lets take it on by one
-    
     private var startNode : Node<T>?
-    
+
     var isEmpty: Bool
     {
         return self.startNode == nil
@@ -25,27 +24,37 @@ class LinkedList<T>: CustomStringConvertible where T:Comparable, T:CustomStringC
         var data = ""
         let top = "### LINK LIST ###\n"
         let bottom = "\n#############\n"
-        
         var node = self.startNode
-        
         while(node != nil)
         {
             data += (node?.data.description)! + "\n"
             node = node?.next
         }
-        
-        
         return top + data + bottom
     }
     
     func append(element:T)
     {
-        
+        let item = Node(data: element)
+        if var node = self.startNode
+        {
+            while(node.next != nil)
+            {
+                node = node.next!
+            }
+            node.next = item
+        }
+        else
+        {
+            self.startNode = item
+        }
     }
     
     func insert(element:T)
     {
-        
+        let item = Node(data: element)
+        item.next = self.startNode
+        self.startNode = item
     }
    
     func insert(position:Int, element:T)
@@ -55,10 +64,19 @@ class LinkedList<T>: CustomStringConvertible where T:Comparable, T:CustomStringC
     func removeFirst()
     {
         
+       self.startNode = self.startNode?.next
     }
     func removeLast()
     {
+      var nextNode = self.startNode
+        var previousNode: Node<T>?
         
+        while (nextNode?.next != nil)
+        {
+            previousNode = nextNode
+            nextNode = nextNode?.next
+        }
+        previousNode?.next = nil
     }
     
     func remove(at position:Int)
@@ -68,7 +86,6 @@ class LinkedList<T>: CustomStringConvertible where T:Comparable, T:CustomStringC
     
     func search(element:T) -> Int
     {
-        
         return 0
     }
     
